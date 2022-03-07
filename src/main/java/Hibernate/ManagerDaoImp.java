d8569576b2b3f9c0196c4bcd1bb1632f460ba90f
Angeudy Brisita Perez <angeudybp@gmail.com>
2022-03-07 13:26:04 -0500

    update

--- src/main/java/Hibernate/ManagerDaoImp.java
+++ src/main/java/Hibernate/ManagerDaoImp.java
@@ -1,30 +1,69 @@
 package Hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 import java.util.List;
 
 public class ManagerDaoImp implements ManagerDao{
     @Override
     public void addManager(Manager manager) {
+        SessionFactory factory = ConnectionFactory.getConfiguration().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
 
+        session.persist(manager);
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void updateManager(Manager manager) {
+        SessionFactory factory = ConnectionFactory.getConfiguration().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.update(manager);
+        transaction.commit();
+        session.close();
+
 
     }
 
     @Override
     public void deleteManager(int id) {
+        SessionFactory factory = ConnectionFactory.getConfiguration().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(getManagerById(id));
+        transaction.commit();
+        session.close();
 
     }
 
     @Override
     public List<Manager> getManagers() {
-        return null;
+        List<Manager> managers;
+        SessionFactory factory = ConnectionFactory.getConfiguration().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        managers= session.createQuery("from Manager",Manager.class).list();
+        transaction.commit();
+        session.close();
+
+        return managers;
     }
 
     @Override
     public Manager getManagerById(int id) {
-        return null;
+        SessionFactory factory = ConnectionFactory.getConfiguration().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Manager manager = session.find(Manager.class,id);
+        transaction.commit();
+        session.close();
+
+        return manager;
     }
 }
--- src/main/java/Hibernate/ReimbursementDaoImp.java
+++ src/main/java/Hibernate/ReimbursementDaoImp.java
@@ -1,30 +1,68 @@
 package Hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 import java.util.List;
 
 public class ReimbursementDaoImp implements ReimbursementDao{
     @Override
     public void addReimbursement(Reimbursement reimbursement) {
+        SessionFactory factory = ConnectionFactory.getConfiguration().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.persist(reimbursement);
+        transaction.commit();
+        session.close();
 
     }
 
     @Override
     public void updateReimbursement(Reimbursement reimbursement) {
+        SessionFactory factory = ConnectionFactory.getConfiguration().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
 
+        session.update(reimbursement);
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void deleteReimbursement(int id) {
+        SessionFactory factory = ConnectionFactory.getConfiguration().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(getReimbursementById(id));
+        transaction.commit();
+        session.close();
 
     }
 
     @Override
     public List<Reimbursement> getEmployees() {
-        return null;
+        List<Reimbursement> reimbursements;
+        SessionFactory factory = ConnectionFactory.getConfiguration().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        reimbursements = session.createQuery("from Reimbursement",Reimbursement.class).list();
+        transaction.commit();
+        session.close();
+
+        return reimbursements;
     }
 
     @Override
     public Reimbursement getReimbursementById(int id) {
-        return null;
+        SessionFactory factory = ConnectionFactory.getConfiguration().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Reimbursement reimbursement = session.find(Reimbursement.class,id);
+        transaction.commit();
+        session.close();
+
+        return reimbursement;
     }
 }

705d6e14bbc7a5c15bf002167bd94a1b7800be24
Angeudy Brisita Perez <angeudybp@gmail.com>
2022-03-07 17:59:36 -0500

    Employee and Manager logout implemented

---
+++ src/main/java/EmployeeLogout.java
@@ -0,0 +1,41 @@
+import Hibernate.Employee;
+import Hibernate.EmployeeDao;
+import Hibernate.EmployeeDaoImp;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class EmployeeLogout extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter printWriter = resp.getWriter();
+
+        req.getRequestDispatcher("index.html").include(req,resp);
+
+
+        HttpSession session = req.getSession();
+        int id = (Integer)session.getAttribute("id");
+        EmployeeDao employeeDao = new EmployeeDaoImp();
+        Employee employee = employeeDao.getEmployeeById(id);
+        employee.setLogged(false);
+        employeeDao.updateEmployee(employee);
+
+
+        session.setAttribute("uname","");
+        session.setAttribute("id",0);
+        printWriter.println("You Successfully logged out!");
+        printWriter.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/ManagerLogout.java
@@ -0,0 +1,39 @@
+import Hibernate.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ManagerLogout extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter printWriter = resp.getWriter();
+
+        req.getRequestDispatcher("index.html").include(req,resp);
+
+
+        HttpSession session = req.getSession();
+        int id = (Integer)session.getAttribute("id");
+        ManagerDao managerDao = new ManagerDaoImp();
+        Manager manager = managerDao.getManagerById(id);
+        manager.setLogged(false);
+        managerDao.updateManager(manager);
+
+
+        session.setAttribute("uname","");
+        session.setAttribute("id",0);
+        printWriter.println("You Successfully logged out!");
+        printWriter.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}

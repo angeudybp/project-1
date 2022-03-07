91dba1795f006c8ad1641c9fb3f43080fd971c5c
Angeudy Brisita Perez <angeudybp@gmail.com>
2022-03-07 13:15:08 -0500

    update

--- src/main/java/Hibernate/EmployeeDaoImp.java
+++ src/main/java/Hibernate/EmployeeDaoImp.java
@@ -1,30 +1,71 @@
 package Hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 import java.util.List;
 
 public class EmployeeDaoImp implements EmployeeDao{
     @Override
     public void addEmployee(Employee employee) {
+        SessionFactory factory = ConnectionFactory.getConfiguration().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.persist(employee);
+        transaction.commit();
+        session.close();
 
     }
 
     @Override
     public void updateEmployee(Employee employee) {
 
+        SessionFactory factory = ConnectionFactory.getConfiguration().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.update(employee);
+        transaction.commit();
+        session.close();
+
     }
 
     @Override
     public void deleteEmployee(int id) {
+        SessionFactory factory = ConnectionFactory.getConfiguration().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(getEmployeeById(id));
+        transaction.commit();
+        session.close();
+
 
     }
 
     @Override
     public List<Employee> getEmployees() {
-        return null;
+        List<Employee> employees;
+        SessionFactory factory = ConnectionFactory.getConfiguration().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        employees = session.createQuery("from Employee",Employee.class).list();
+        transaction.commit();
+        session.close();
+
+        return employees;
     }
 
     @Override
     public Employee getEmployeeById(int id) {
-        return null;
+        SessionFactory factory = ConnectionFactory.getConfiguration().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Employee employee = session.find(Employee.class,id);
+        transaction.commit();
+        session.close();
+
+        return employee;
     }
 }
